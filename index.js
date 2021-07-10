9dfdceed0eab4f5b6548b7a537b39fb7fc9fcace
Richard Hess <eswat2@gmail.com>
2021-07-10 11:27:17 -0700

    expand map functionality

--- index.js
+++ index.js
@@ -5,44 +5,57 @@
  */
 const plugin = require('tailwindcss/plugin')
 
-// NOTE:  the base color names come from https://clrs.cc
-const NAMES = [
-  'navy',
-  'blue',
-  'aqua',
-  'teal',
-  'olive',
-  'green',
-  'lime',
-  'yellow',
-  'orange',
-  'red',
-  'maroon',
-  'fuchsia',
-  'purple',
-  'black',
-  'gray',
-  'silver',
-]
+// NOTE:  the base color names & values come from https://clrs.cc
+const COLOR_MAP = {
+  navy: '001f3f',
+  blue: '0074d9',
+  aqua: '7fdbff',
+  teal: '39cccc',
+  olive: '3d9970',
+  green: '2ecc40',
+  lime: '01ff70',
+  yellow: 'ffdc00',
+  orange: 'ff851b',
+  red: 'ff4136',
+  maroon: '85144b',
+  fuchsia: 'f012be',
+  purple: 'b10dc9',
+  black: '111111',
+  gray: 'aaaaaa',
+  silver: 'dddddd',
+}
+
+const NAMES = Object.keys(COLOR_MAP)
 
 const PREFIX = 'clrs'
 
 const getColors = (options) => {
+  const tags = Object.keys(options)
+
   const colorObj = {}
+  const fallbacks = tags.includes('map') || tags.includes('alphas')
+
+  const map = (options && options['map']) || COLOR_MAP
+  const alphas = (options && options['alphas']) || []
+
   const base = (options && options['names']) || NAMES
-  const map = (options && options['map']) || undefined
   const prefix = (options && options['prefix']) || PREFIX
   const extras = (options && options['extras']) || []
   const skip = (options && options['skip']) || []
   const variants = (options && options['variants']) || []
 
-  if (map) {
+  if (fallbacks) {
     const keys = Object.keys(map)
 
     keys.forEach((key) => {
       const name = `${prefix}-${key}`
-      const value = map[key];
+      const value = map[key]
       colorObj[name] = value ? `var(--${name}, ${value})` : `var(--${name})`
+      alphas.forEach((av) => {
+        const next = `${name}-a${av}`
+        const colr = `${value}${av}`
+        colorObj[next] = `var(--${next}, ${colr})`
+      })
     })
   } else {
     const names = [...base, ...extras]
--- test.js
+++ test.js
@@ -158,22 +158,43 @@ test('map can define vars and default colors', () => {
   return generatePluginCss(null, {
     prefix: 'ds1',
     map: {
-      'primary': '#ff0000'
+      primary: '#ff0000',
     },
   }).then((css) => {
-    const vars = css.match(/var\(--ds1-.*\)/g)
-    const total = css.match(/--ds1-[a-z]*[1-9]*(-(?:light|dark))?/g)
+    const total = css.match(/--ds1-[a-z]*[1-9]*/g)
     const tags = Array.from(new Set(total))
-    const list = css.match(/\.[a-z]*-ds1-[a-z]*[1-9]*(-(?:light|dark))?/g)
-    const results = [
-      '--ds1-primary',
-    ]
-    console.log(vars)
+    const list = css.match(/\.[a-z]*-ds1-[a-z]*[1-9]*/g)
+    const results = ['--ds1-primary']
 
     expect(tags).toEqual(expect.arrayContaining(results))
     expect(tags.length).toEqual(1)
 
     expect(list).toEqual(expect.arrayContaining(['.text-ds1-primary']))
     expect(list.length).toEqual(9)
+  })
+})
+
+test('map can define vars and default colors with alphas', () => {
+  return generatePluginCss(null, {
+    map: {
+      bada55: 'bada55',
+      slate: '708090',
+      slate4: '4e5964',
+      white: 'ffffff',
+    },
+    alphas: [50],
+  }).then((css) => {
+    const total = css.match(/--clrs-[a-z]*[1-9]*(-(?:a50))?/g)
+    const tags = Array.from(new Set(total))
+    const list = css.match(/\.[a-z]*-clrs-[a-z]*[1-9]*(-(?:a50))?/g)
+    const results = ['--clrs-bada55', '--clrs-bada55-a50']
+
+    expect(tags).toEqual(expect.arrayContaining(results))
+    expect(tags.length).toEqual(8)
+
+    expect(list).toEqual(
+      expect.arrayContaining(['.text-clrs-bada55', '.text-clrs-bada55-a50'])
+    )
+    expect(list.length).toEqual(72)
   })
 })
